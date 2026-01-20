e5fa8049269ad34b74f5ca6b5b911cf3416ef51f
ivdobry <ivan@rzilient.club>
2026-01-20 18:59:24 +0100

    Capture mouse when click input if mouse is mode visible.

--- player/player.gd
+++ player/player.gd
@@ -56,6 +56,9 @@ func _input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	if event.is_action_pressed("fire") && Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		
 func _handle_camera_rotation() -> void:
 	rotate_y(mouse_motion.x)
